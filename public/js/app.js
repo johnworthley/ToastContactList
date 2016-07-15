11576ab7269d05c2e0e30512df20e28a6641cb9a
Michael Steward <michael.steward4@gmail.com>
2016-07-14 23:47:35 -0400

    Set up payPal to appear as modal dialog when user clicks the buy button. The price of the tile you clicked buy on is passed to the dialog.

--- public/js/app.js
+++ public/js/app.js
@@ -1,11 +1,35 @@
-(function(){
-var myApp = angular.module('toastApp',[])
-    .controller("TileController",['$scope','$http', function($scope, $http) {
+myApp = angular.module('toastApp',['ui.bootstrap','ngAnimate']);
+myApp.controller("TileController",['$scope','$http','$uibModal',function($scope, $http, $uibModal) {
         $http.get("../data/tile_data.json").then(function(tileData) {
             $scope.myTiles = tileData.data;
         });
         $scope.hideTile = function(tile) {
             tile.showTile = false;
         };
+
+       $scope.loadPayPartial = function(tile) {
+           var modalInstance = $uibModal.open({
+                animation: true,
+                templateUrl: 'pay.html',
+                backdrop: 'static',
+                resolve: {
+                    amount: function() {
+                        return tile.price;
+                    }
+                },
+                controller: 'ModalInstanceCtrl'               
+            });
+        };
+           
     }]);
-})();
+    
+myApp.controller('ModalInstanceCtrl',['$scope','$uibModalInstance','amount', function ($scope, $uibModalInstance, amount) {
+       $scope.amount = amount;
+        $scope.ok = function () {
+             $uibModalInstance.dismiss('cancel');
+        };
+
+        $scope.cancel = function () {
+            $uibModalInstance.dismiss('cancel');
+        };
+    }]);
