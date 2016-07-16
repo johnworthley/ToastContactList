1c84f233612b3c6dc4f737ce852d3db00620f613
Michael Steward <michael.steward4@gmail.com>
2016-07-16 03:38:40 -0400

    Added "Add Tile" modal dialog box, and button to create new tile.

--- public/js/app.js
+++ public/js/app.js
@@ -1,35 +1,69 @@
 myApp = angular.module('toastApp',['ui.bootstrap','ngAnimate']);
 myApp.controller("TileController",['$scope','$http','$uibModal',function($scope, $http, $uibModal) {
-        $http.get("../data/tile_data.json").then(function(tileData) {
-            $scope.myTiles = tileData.data;
+    $http.get("../data/tile_data.json").then(function(tileData) {
+        $scope.myTiles = tileData.data;
+    });
+    $scope.hideTile = function(tile) {
+        tile.showTile = false;
+    };
+
+    $scope.loadPayPartial = function(tile) {
+        var modalInstance = $uibModal.open({
+            animation: true,
+            templateUrl: 'pay.html',
+            backdrop: 'static',
+            resolve: {
+                amount: function() {
+                    return tile.price;
+                }
+            },
+            controller: 'PayModalInstanceCtrl'               
         });
-        $scope.hideTile = function(tile) {
-            tile.showTile = false;
-        };
+    };
 
-       $scope.loadPayPartial = function(tile) {
-           var modalInstance = $uibModal.open({
-                animation: true,
-                templateUrl: 'pay.html',
-                backdrop: 'static',
-                resolve: {
-                    amount: function() {
-                        return tile.price;
-                    }
-                },
-                controller: 'ModalInstanceCtrl'               
-            });
-        };
+    $scope.loadAddTilePartial = function () {
+        var modalInstance = $uibModal.open({
+            animation: true,
+            templateUrl: 'addTile.html',
+            backdrop: 'static',
+            controller: 'AddTileModalInstanceCtrl'
+        });    
+    };
            
-    }]);
+}]);
     
-myApp.controller('ModalInstanceCtrl',['$scope','$uibModalInstance','amount', function ($scope, $uibModalInstance, amount) {
-       $scope.amount = amount;
-        $scope.ok = function () {
-             $uibModalInstance.dismiss('cancel');
-        };
+myApp.controller('PayModalInstanceCtrl',['$scope','$uibModalInstance','amount', function ($scope, $uibModalInstance, amount) {
+    $scope.amount = amount;
+    $scope.ok = function () {
+        $uibModalInstance.dismiss('cancel');
+    };
 
-        $scope.cancel = function () {
-            $uibModalInstance.dismiss('cancel');
-        };
-    }]);
+    $scope.cancel = function () {
+        $uibModalInstance.dismiss('cancel');
+    };
+}]);
+
+myApp.controller('AddTileModalInstanceCtrl',['$scope','$uibModalInstance', function($scope,$uibModalInstance){
+    $scope.tile = [{
+        "title" : '',
+        "price" : '',
+        "where" : '',
+        "day" : {
+            "su" : '',
+            "mu" : '',
+            "tu" : '',
+            "we" : '',
+            "th" : '',
+            "fr" : '',
+            "sa" : '' },
+        "time" : ''
+    }];
+
+    $scope.submit = function () {
+        $uibModalInstance.dismiss('cancel');
+    };
+
+    $scope.cancel = function () {
+        $uibModalInstance.dismiss('cancel');
+    };
+}]);
